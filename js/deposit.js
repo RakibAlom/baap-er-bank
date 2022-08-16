c8fcd428de9038810c52354dc15d60259ac028ca
RakibAlom <rakibalom17@gmail.com>
2022-08-17 05:20:36 +0600

    Project Done

--- js/deposit.js
+++ js/deposit.js
@@ -1,6 +1,27 @@
-// Deposit Event Handler
+let inputDepositAmount = document.getElementById('deposit-amount');
 let depositSubmit = document.getElementById('depositSubmit');
+
+// Enable Deposit Button
+inputDepositAmount.addEventListener('keyup', function() {
+  if(inputDepositAmount.value != "") {
+    depositSubmit.removeAttribute('disabled');
+  } else {
+    depositSubmit.setAttribute('disabled', true);
+  }
+});
+
+// Deposit Event Handler
 depositSubmit.addEventListener('click',function() {
+  // Get New Input Amount
+  let newInputAmount = document.getElementById('deposit-amount');
+  let newAmount = parseFloat(newInputAmount.value);
+  newInputAmount.value = '';
+  depositSubmit.setAttribute('disabled', true);
+  if(isNaN(newAmount)) {
+    return alert('Please, enter valid number')
+  }
+
+
   // Get Previous Deposit Amount
   let getDepositAmount = document.getElementById('depositAmount');
   let depositAmount = parseFloat(getDepositAmount.innerText);
@@ -9,14 +30,16 @@ depositSubmit.addEventListener('click',function() {
   let getCurrentAmount = document.getElementById('currentAmount');
   let currentAmount = parseFloat(getCurrentAmount.innerText);
 
-  // Get New Input Amount
-  let newInputAmount = document.getElementById('deposit-amount').value;
-  let newAmount = parseFloat(newInputAmount);
-
   // Get Total Deposit Amount
   let totalDepositAmount = depositAmount + newAmount;
 
   // Show Total Deposit Amount & Current Ammount
   getDepositAmount.innerText = totalDepositAmount;
   getCurrentAmount.innerText = currentAmount + newAmount;
+
+  // show alert
+  let alert = document.getElementById('alert');
+  let alertText = document.getElementById('alert-text');
+  alert.classList.remove('hidden');
+  alertText.innerText = `$${newAmount} deposit successfully done from your account`;
 })
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,53 @@
+let inputAmount = document.getElementById('withdraw-amount');
+let withdrawSubmit = document.getElementById('withdrawSubmit');
+
+// Enable Withdraw Button
+inputAmount.addEventListener('keyup', function () {
+  if(inputAmount.value != ""){
+    withdrawSubmit.removeAttribute('disabled');
+  } else {
+    withdrawSubmit.setAttribute('disabled', true);
+  }
+})
+
 // Withdraw Event Handler
+withdrawSubmit.addEventListener('click', function() {
+  // Get Withdraw Input Amount
+  let newInputAmount = document.getElementById('withdraw-amount');
+  let newWithdrawAmount = parseFloat(newInputAmount.value);
+  newInputAmount.value = '';
+  withdrawSubmit.setAttribute('disabled', true);
+
+  // If Input Value Not Pure Number
+  if(isNaN(newWithdrawAmount)) {
+    return alert('please, enter valid number')
+  }
+
+  // Get Previous Withdraw Amount
+  let getWithdrawAmount = document.getElementById('withdrawAmount');
+  let withdrawAmount = parseFloat(getWithdrawAmount.innerText);
+
+  // Get Current Amount
+  let getCurrentAmount = document.getElementById('currentAmount');
+  let currentAmount = parseFloat(getCurrentAmount.innerText);
+
+   // If Withdraw Amount More Then Current Ammount
+   if(newWithdrawAmount > currentAmount) {
+    return alert('please, you have not enough amount!');
+  }
+
+  // Get Total Withdraw Amount
+  let totalWithdrawAmount = withdrawAmount + newWithdrawAmount;
+  let totalCurrentAmmount = currentAmount - newWithdrawAmount;
+
+  // Show Total Withraw & Current Amount 
+  getWithdrawAmount.innerText = totalWithdrawAmount;
+  getCurrentAmount.innerText = totalCurrentAmmount;
+
+  // show alert
+  let alert = document.getElementById('alert');
+  let alertText = document.getElementById('alert-text');
+  alert.classList.remove('hidden');
+  alertText.innerText = `$${newWithdrawAmount} Withdraw successfully done from your account`;
+  
+})
